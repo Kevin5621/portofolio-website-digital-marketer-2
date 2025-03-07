5cbae31f4c27fc55501f2c190780413dfe66378d
Yohanes Kevin <yohanes123kevin123@gmail.com>
2025-03-07 09:38:00 +0700

    fix - route

--- src/app/project/2-ortist-specialist/page.tsx
+++
@@ -1,5 +0,0 @@
-// import OrtistSpecialist from '@/components/project-detail/2-ortist';
-
-// export default function OrtistSpecialistPage() {
-//   return <OrtistSpecialist />;
-// }
---
+++ src/app/project/ortist/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from '@/components/navbar';
+import Ortist from '@/components/project-detail/2-ortist';
+
+export default function OrtistPage() {
+  return (
+    <main className="min-h-screen">
+      <Navbar />
+      <Ortist />
+    </main>
+  );
+}
